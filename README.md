f1fe379ca201127f9d87ed42c1e109a775eaf90d
Kamil Banach <kontakt@elwin013.com>
2022-11-27 12:27:12 +0100

    initial code - deps, single endpoint

---
+++ README.md
@@ -0,0 +1,1 @@
+# Project Yala - Yet Another Link-shortener App
---
+++ src/main/java/com/elwin013/yala/App.java
@@ -0,0 +1,11 @@
+package com.elwin013.yala;
+
+import io.javalin.Javalin;
+
+public class App {
+    public static void main(String[] args) {
+        var app = Javalin.create(/*config*/)
+                .get("/", ctx -> ctx.result("Hello World"))
+                .start(7070);
+    }
+}
